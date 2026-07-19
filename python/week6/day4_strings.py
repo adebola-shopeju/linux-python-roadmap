2f60ce7ca8a6e972a6e822a4683d22b580267aba
Adebola Shopeju <sadebola@nmsu.edu>
2026-07-19 16:44:49 +0100

    day4_strings.py

---
+++ python/week6/day4_strings.py
@@ -0,0 +1,35 @@
+name = "Adebola"
+print(name)
+
+print(name.upper())
+country = "NIGERIA"
+
+print(country)
+print(country.lower())
+
+username = "  Adebola  "
+print(username)
+
+print(username.strip())
+cloud = "I am learning Azure"
+
+print(cloud)
+print(cloud.replace("Azure", "AWS"))
+
+server = "aws-production-server"
+print(server.startswith("aws"))
+
+filename = "backup.zip"
+print(filename.endswith(".zip"))
+
+aws_resource = "  EC2-instance-production  "
+print(aws_resource)
+
+aws_resource = "EC2-instance-production"
+print(aws_resource.lower())
+
+aws_resource = "ec2-instance-production"
+print(aws_resource.replace("ec2", "aws-ec2"))
+
+aws_resource = "aws-ec2-instance-production"
+print(aws_resource.startswith("aws"))
